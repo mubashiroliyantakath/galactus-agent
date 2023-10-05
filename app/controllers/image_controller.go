c401f14adb2ff3e12662fa724d064a2d6314de5c
MubashirOliyantakath <mubashi.r@hotmail.com>
2023-10-05 20:54:34 +1100

    Added container actions APIs

--- app/controllers/image_controller.go
+++ app/controllers/image_controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
@@ -9,17 +10,21 @@ import (
 	"github.com/mubashiroliyantakath/galactus-agent/internal/utils/logging"
 )
 
-func ImageController(c *fiber.Ctx) error {
+func ImageList(c *fiber.Ctx) error {
+	logging.Log.Debug("Fetching Image List")
 	dockerClient, err := client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
-		logging.Log.Info("Error creating docker client")
+		logging.Log.Error("Error creating docker client")
 		return fiber.ErrInternalServerError
 	}
+	defer dockerClient.Close()
+
 	images, err := dockerClient.ImageList(context.Background(), types.ImageListOptions{All: true})
 	if err != nil {
 		return fiber.ErrInternalServerError
 	}
-	dockerClient.Close()
+
+	logging.Log.Debug(fmt.Sprintf("Image list fetched: %v", images))
 	return c.JSON(images)
 
 }
