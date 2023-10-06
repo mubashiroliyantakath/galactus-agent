4e481013d0bb323e135849133fa81db7c3211b23
MubashirOliyantakath <mubashi.r@hotmail.com>
2023-10-06 22:20:55 +1100

    Image actions on galactus dashboard

--- web/app/api/containers/action/route.js
+++ web/app/api/containers/action/route.js
@@ -1,6 +1,3 @@
-export const dynamic = 'force-dynamic'
-import { NextResponse } from "next/server";
-
 export async function POST(request) {
         const res = await request.json()
         const requestOptions = {
@@ -11,7 +8,7 @@ export async function POST(request) {
                 body: JSON.stringify(res)
             }
         const result = await fetch(`${process.env.GALACTUS_AGENT_API}/api/v1/containers/action`, requestOptions)
-        // const res = await fetch(`${process.env.GALACTUS_AGENT_API}/api/v1/containers/action`,{ cache: 'no-store' })
+
         if (result.status === 200) {
                 return new Response("Done", {status: 200})
         }
